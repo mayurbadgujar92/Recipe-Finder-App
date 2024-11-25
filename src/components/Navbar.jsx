0dc3b54d4c19b8ffbe14c5020870550daaeb93cc
mayurbadgujar92 <159112885+mayurbadgujar92@users.noreply.github.com>
2024-11-25 12:58:41 +0530

    jsx file

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div id='logo'>
+        <img src="/diet.png" alt="logo" width={50} />
+      <h2>Recipe Finder App</h2>
+      </div>
+      <Link to={"/"}>
+
+        Home</Link>
+        {/* <Footer/> */}
+    </nav>
+  )
+}
+
+export default Navbar
